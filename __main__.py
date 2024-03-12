42f54037f6917882ac07958b38ea9f9db56bc443
paduy2 <paduy2@tma.com.vn>
2024-03-12 11:48:01 +0700

    Duy commit script

--- __main__.py
+++ __main__.py
@@ -4,7 +4,35 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('pulumi-my-bucket')
+bucket = s3.Bucket('pulumi-my-bucket',
+    website=s3.BucketWebsiteArgs(
+      index_document="index.html",
+    )
+)
 
 # Export the name of the bucket
-pulumi.export('pulumi-bucket_name', bucket.id)
+pulumi.export('pulumi_bucket_name', bucket.id)
+
+ownership_controls = s3.BucketOwnershipControls(
+    'ownership-controls',
+    bucket=bucket.id,
+    rule=s3.BucketOwnershipControlsRuleArgs(
+        object_ownership='ObjectWriter',
+    ),
+)
+
+public_access_block = s3.BucketPublicAccessBlock(
+    'public-access-block', bucket=bucket.id, block_public_acls=False
+)
+
+#Create bucket object
+bucket_object = s3.BucketObject(
+    'index.html',
+    bucket=bucket.id,
+    source=pulumi.FileAsset('./html/index.html'),
+    content_type='text/html',
+    acl='public-read',
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block, ownership_controls]),
+)
+
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
